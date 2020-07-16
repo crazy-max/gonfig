55d6cd2b43bdd962a177b1674831783a736a52df
CrazyMax <crazy-max@users.noreply.github.com>
2020-07-16 10:23:44 +0200

    Typo

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 ## About
 
-This library is largely inspired by the traefik config package. It offers a simple and performant config handling with:
+This library is heavily inspired by the traefik config package. It offers a simple and performant config handling with:
 
 * Configuration files in YAML or TOML format
 * Environmental variables
--- gonfig.go
+++ gonfig.go
@@ -1,6 +1,6 @@
 // Lightweight config handling for Go.
 //
-// This library is largely inspired by the traefik config package. It offers a simple and
+// This library is heavily inspired by the traefik config package. It offers a simple and
 // performant config handling with:
 // - Configuration files in YAML or TOML format
 // - Environmental variables
