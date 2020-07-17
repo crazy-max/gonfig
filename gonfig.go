224fe76330e831ffa67de5437166fe74b4974e19
CrazyMax <crazy-max@users.noreply.github.com>
2020-07-17 11:44:57 +0200

    Typo

--- gonfig.go
+++ gonfig.go
@@ -3,7 +3,7 @@
 // This library is heavily inspired by the traefik config package. It offers a simple and
 // performant config handling with:
 // - Configuration files in YAML or TOML format
-// - Environmental variables
+// - Environment variables
 // - Flag arguments
 //
 // Get started:
